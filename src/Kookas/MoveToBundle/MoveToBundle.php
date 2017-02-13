2fff840349805a2ef97c85e759d809c68ecb033b
kookas <kookas.mail@gmail.com>
2017-02-13 03:02:01 +0000

    Updated bundle def file.

--- src/Kookas/MoveToBundle/MoveToBundle.php
+++ src/Kookas/MoveToBundle/MoveToBundle.php
@@ -1,9 +1,26 @@
 <?php
 
+/**
+ * This file is part of kookas/movetobundle.
+ *
+ * (c) Ashleigh Udoh <kookas.mail@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Kookas\MoveToBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class MoveToBundle extends Bundle
+/**
+ * KookasMoveToBundle
+ *
+ * @package    kookas/moveto
+ * @author     Ashleigh Udoh <kookas.mail@gmail.com>
+ * @copyright  2017 Ashleigh Udoh
+ * @license    http://www.opensource.org/licenses/MIT The MIT License
+ */
+class KookasMoveToBundle extends Bundle
 {
 }
